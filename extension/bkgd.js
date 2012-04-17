df2d9546e19d808c8998116a0af053497af35c30
Jerry B <jerry@noisenewyork.com>
2012-04-16 23:40:27 -0400

    Add quizlet api

--- extension/bkgd.js
+++ extension/bkgd.js
@@ -1,16 +1,31 @@
+var notSoFast = {};
+notSoFast.white_list = [];
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	var blocked_websites, website, tabUrl = changeInfo.url;
 	
-	if(tabUrl && tabUrl.indexOf('?passed=true') == -1){
-		tabUrl = tabUrl.replace('http://', '').replace('https://', '');
-		blocked_websites = JSON.parse(localStorage.getItem('blocked_websites'));
-		
-		for(var i=0; i<blocked_websites.length; i++){
-			website = blocked_websites[i];
-			
-			if(tabUrl.substring(0, website.length) == website){
-				chrome.tabs.update(tab.id, {url: 'http://brianmeise.com/plugin?url=' + changeInfo.url});
+	if(tabUrl){
+		for(var i=0; i<notSoFast.white_list.length; i++){
+			if(notSoFast.white_list[i] == tabId){
+				return;
 			}
+		}
+		
+		if(tabUrl.indexOf('?passed=true') == -1){
+			tabUrl = tabUrl.replace('http://', '').replace('https://', '');
+			blocked_websites = JSON.parse(localStorage.getItem('blocked_websites'));
+			
+			for(var j=0; i<blocked_websites.length; j++){
+				website = blocked_websites[j];
+				
+				if(tabUrl.substring(0, website.length) == website){
+					chrome.tabs.update(tab.id, {url: 'chrome-extension://koinhggfjpkmamkfhooibemobpdbdbcc/quiz.html?url=' + changeInfo.url});
+				}
+			}
+		}
+		else if(tabUrl.indexOf('?passed=true') >= 0){
+			notSoFast.white_list.push(tabId);
+			console.log(notSoFast.white_list);
 		}
 	}
 });
--- extension/settings.js
+++ extension/settings.js
@@ -8,7 +8,17 @@ var settings = {};
 		settings.getWebsites();
 		settings.updateWebsites();
 		
+		var current_quiz = JSON.parse(localStorage.getItem('current_quiz'));
+		if(current_quiz){
+			$('.quiz-current').html('Current: ' + current_quiz.title);
+		}
+		else{
+			$('.quiz-current').html('Current: No Quiz');
+		}
+		
 		$('.website-button').on('click', settings.storeWebsite);
+		$('.quiz-button').on('click', settings.searchQuizzes);
+		$('.quiz-list a').live('click', settings.setQuiz);
 	};
 	
 	settings.getWebsites = function(){
@@ -39,6 +49,39 @@ var settings = {};
 		else{
 			alert('NOT VALID!!!!!!!');
 		}
+		
+		return false;
+	};
+	
+	settings.searchQuizzes = function(){
+		var term = $('.quiz-input').val(), list = $('.quiz-list');
+		
+		if(term.length >= 3){
+			$.get('https://api.quizlet.com/2.0/search/sets?q='+term+'&client_id=DvyRJVGqjG', {}, function(response){
+				var sets = response.sets;
+				if(sets){
+					list.empty();
+					for(var i=0; i<sets.length; i++){
+						list.append('<li><a data-id="' + sets[i].id + '" href="#">' + sets[i].title + '</a></li>');
+					}
+				}
+			});
+		}
+		else{
+			alert('SEARCH TERM MUST BE 3 CHARACTERS OR MORE!');
+		}
+		
+		return false;
+	};
+	
+	settings.setQuiz = function(){
+		var _this = $(this), id, title;
+		id = _this.attr('data-id');
+		title = _this.html();
+		
+		$('.quiz-current').html('Current: ' + title);
+		
+		localStorage.setItem('current_quiz', JSON.stringify({id: id, title: title}));
 		
 		return false;
 	};
